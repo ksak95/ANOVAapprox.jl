7bcb7bb86dd3769c3732a811634580f5a13e1069
ksak <kseniya.akhalaya@s2016.tu-chemnitz.de>
2024-05-24 16:03:05 +0200

    changes

--- src/errors.jl
+++ src/errors.jl
@@ -173,7 +173,6 @@ end
 
 function get_acc(a::approx, λ::Float64)::Float64
     y_eval = evaluate(a, λ)
-    #return mean(y_eval .== a.y) * 100.00
     return count(sign.(y_eval) .== a.y)/length(y)*100.00
 end
 
@@ -184,8 +183,7 @@ function get_acc(
     λ::Float64,
 )::Float64
     y_eval = evaluate(a, X, λ)
-    return mean(y_eval .== y) * 100.00
-    #return count(sign.(y_eval) .== y)/length(y)*100.0
+    return count(sign.(y_eval) .== y)/length(y)*100.0
 end
 
 function get_acc(a::approx)::Dict{Float64,Float64}
